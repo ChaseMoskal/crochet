6cbda4a5fdf2657927141bbb73d9b219c069843e
Chase Moskal <chasemoskal@gmail.com>
2017-03-18 12:17:59 -0700

    Establish concept for Crochet class

--- source/crochet.ts
+++ source/crochet.ts
@@ -1,7 +1,31 @@
 
-export { default as evaluate } from "./evaluate"
-export * from "./evaluate"
+export interface CrochetOptions {
+  root: string
+}
 
-export * from "./disk/directories"
-export * from "./disk/files"
-export * from "./disk/paths"
+export default class Crochet {
+  root: string
+
+  constructor({root = "."}: CrochetOptions) {
+    this.root = root
+  }
+
+  async pages({template, source, out, home, context}: CrochetPagesOptions) {}
+
+  async blog({template, source, out, context}: CrochetBlogOptions) {}
+}
+
+export interface CrochetPagesOptions {
+  template: string
+  source: string
+  out: string
+  home: string
+  context: any
+}
+
+export interface CrochetBlogOptions {
+  template: string
+  source: string
+  out: string
+  context: any
+}
