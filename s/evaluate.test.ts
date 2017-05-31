2bcb452cc3acd5088ed0ec71126b215c8826a2f2
Chase Moskal <lordbrimshaw@gmail.com>
2017-05-30 20:38:00 -0700

    use blue-tape instead of tape

--- s/evaluate.test.ts
+++ s/evaluate.test.ts
@@ -1,42 +1,35 @@
 
-import * as test from "tape"
+import * as test from "blue-tape"
 import evaluate from "./evaluate"
 
 test("evaluate blocks with <?js and <?", async t => {
   t.equal(await evaluate("<?js()=>1?>"), "1")
   t.equal(await evaluate("<?()=>1?>"), "1")
   t.equal(await evaluate("<?()=>\n1\n?>"), "1", "multiline blocks")
-  t.end()
 })
 
 test("evaluate a value", async t => {
   t.equal(await evaluate("abc <?2+2?> xyz"), "abc 4 xyz")
-  t.end()
 })
 
 test("evaluate nothing", async t => {
   t.equal(await evaluate("abc"), "abc")
-  t.end()
 })
 
 test("evaluate twice", async t => {
   t.equal(await evaluate("abc <?2+2?> and <?4*4?> xyz"), "abc 4 and 16 xyz")
-  t.end()
 })
 
 test("evaluate an arrow function", async t => {
   t.equal(await evaluate("abc <?()=>2+2?> xyz"), "abc 4 xyz")
-  t.end()
 })
 
 test("evaluate a normal function", async t => {
   t.equal(await evaluate("abc <?function(){return 2+2}?> xyz"), "abc 4 xyz")
-  t.end()
 })
 
 test("evaluate a promise", async t => {
   t.equal(await evaluate("<?()=>new Promise(r=>setTimeout(()=>r(9),10))?>"), "9")
-  t.end()
 })
 
 test.skip("evaluate an async function", async t => {
@@ -44,15 +37,12 @@ test.skip("evaluate an async function", async t => {
     "abc <?async function(){return new Promise(r=>setTimeout(()=>r(9),10))}?> xyz"),
     "abc 9 xyz"
   )
-  t.end()
 })
 
 test("evaluate provides context to function", async t => {
   t.equal(await evaluate("<?context=>context.lol?>", {lol: "rofl"}), "rofl")
-  t.end()
 })
 
 test("evaluate provides context to value", async t => {
   t.equal(await evaluate("<?context.lol?>", {lol: "rofl"}), "rofl")
-  t.end()
 })
